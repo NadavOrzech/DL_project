75f357beb449e841faae5a431f253e4759a58226
NadavOrzech <45461272+NadavOrzech@users.noreply.github.com>
2020-08-28 15:05:25 +0300

    preprocess of the preprocess

--- source/record.py
+++ source/record.py
@@ -6,10 +6,12 @@ from ecg_tmp import *
 
 def get_ecg_data(datfile): 
     ## convert .dat/q1c to numpy arrays
-    recordname=os.path.basename(datfile).split(".dat")[0]
+    # recordname=os.path.basename(datfile).split(".dat")[0]
+    recordname = datfile.split(".dat")[0]
+
     recordpath=os.path.dirname(datfile)
     cwd=os.getcwd()
-    os.chdir(recordpath) ## somehow it only works if you chdir. 
+    # os.chdir(recordpath) ## somehow it only works if you chdir.
 
     annotator='atr'
     annotation = wfdb.rdann(recordname, extension=annotator, sampfrom=0,sampto = None, pbdir=None)
@@ -17,7 +19,7 @@ def get_ecg_data(datfile):
 
     Vctrecord=np.transpose(record.p_signals)
     VctAnnotationHot=np.zeros( (2,len(Vctrecord[1])), dtype=np.int)
-    VctAnnotationHot[1] = 1;
+    VctAnnotationHot[1] = 1
     
     #print("ecg, 2 lead of shape" , Vctrecord.shape) 
     #print("VctAnnotationHot of shape" , VctAnnotationHot.shape) 
@@ -27,10 +29,11 @@ def get_ecg_data(datfile):
     VctAnnotations=list(zip(annotation.sample,annotation.symbol)) ## zip coordinates + annotations (N),(t) etc)
     #print(VctAnnotations)
     for i in range(len(VctAnnotations)):
+        print(VctAnnotations[i])
         if( VctAnnotations[i][1] == 'N' or 
-            VctAnnotations[i][1] == 'L' or  
-            VctAnnotations[i][1] == 'R' or 
-           VctAnnotations[i][1] == 'B' or 
+            VctAnnotations[i][1] == 'L' or
+            VctAnnotations[i][1] == 'R' or
+           VctAnnotations[i][1] == 'B' or
            VctAnnotations[i][1] == 'A' or
            VctAnnotations[i][1] == 'a' or 
            VctAnnotations[i][1] == 'J' or 
@@ -58,22 +61,23 @@ def get_ecg_data(datfile):
 class Record:
     def __init__(self):
         self.file_name = "C:\\Users\\Dell\\Desktop\\Technion\\DeepLearning\\project_data\\mit-bih\\files\\04043"
-        self.record = wfdb.rdrecord(self.file_name)
+        # self.record = wfdb.rdrecord(self.file_name)
+        self.record = wfdb.rdsamp(self.file_name)
         self.ann = wfdb.rdann(self.file_name,extension="atr")
 
 
     def visualization(self):
-        # wfdb.plot_wfdb(record=self.record, title='test1')
-        # display(self.record.__dict__)
-        wfdb.plot_all_records("C:\\Users\\Dell\\Desktop\\Technion\\DeepLearning\\project_data\\mit-bih\\files")
+        wfdb.plotrec(self.record, annotation = self.ann, title='Record 100 from MIT-BIH Arrhythmia Database', timeunits = 'seconds', figsize = (10,4), ecggrids = 'all')
+        display(self.record.__dict__)
+        # wfdb.plot_all_records("C:\\Users\\Dell\\Desktop\\Technion\\DeepLearning\\project_data\\mit-bih\\files")
 
 
 if __name__ == '__main__':
-    # r = Record()
+    r = Record()
     # r.visualization()
 
     # get_ecg_data("C:\\Users\\Dell\\Desktop\\Technion\\DeepLearning\\project_data\\mit-bih\\files\\04043.dat")
 
-    get_ecg_data("C:\\Users\\Dell\\Desktop\\Technion\\DeepLearning\\project_data\\mit-bih\\files\\04043")
+    get_ecg_data("C:\\Users\\Dell\\Desktop\\Technion\\DeepLearning\\project_data\\mit-bih\\files\\04936.dat")
 
     aaa=2
