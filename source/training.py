1bc97052893163aa100f20cfd93c616a39d387bf
englender <ronien262@gmail.com>
2020-09-05 13:50:31 +0300

    added print metrics for training

--- source/training.py
+++ source/training.py
@@ -15,7 +15,7 @@ def train(model, optimizer, loss_fn, dataloader, max_epochs=4, max_batches=200):
     for epoch_idx in range(max_epochs):
         total_loss, num_correct = 0, 0
         start_time = time.time()
-
+        tp_tot, fp_tot, tn_tot, fn_tot = 0, 0, 0, 0
         for batch_idx, batch in enumerate(dataloader):
             X, y = batch[0], batch[1]
 
@@ -35,13 +35,21 @@ def train(model, optimizer, loss_fn, dataloader, max_epochs=4, max_batches=200):
             # Calculate accuracy
             total_loss += loss.item()
             y_pred = torch.argmax(last_output, dim=1)
+            tp, fp, tn, fn = calculate_acc(y_pred, y)
+            tp_tot += tp
+            fp_tot += fp
+            tn_tot += tn
+            fn_tot += fn
             num_correct += torch.sum(y_pred == y).float().item()
-
-            if batch_idx == max_batches - 1:
-                break
+            total_samp = tp_tot + tn_tot + fp_tot + fn_tot
+            # if batch_idx == max_batches - 1:
+            #     break
 
         print(
-            f"Epoch #{epoch_idx}, loss={total_loss / (max_batches):.3f}, accuracy={num_correct / (max_batches * BATCH_SIZE):.3f}, elapsed={time.time() - start_time:.1f} sec")
+            f"Epoch #{epoch_idx}, loss={total_loss / (total_samp / BATCH_SIZE):.3f}, accuracy={num_correct / total_samp:.3f}, elapsed={time.time() - start_time:.1f} sec")
+        print(f"tp: {tp_tot}, fp: {fp_tot}, tn: {tn_tot}, fn: {fn_tot}")
+        print(f"Positive accuracy: {tp_tot / (tp_tot + fn_tot)}")
+        print(f"Negative accuracy: {tn_tot / (tn_tot + fp_tot)}")
 
 
 def get_model():
@@ -49,6 +57,20 @@ def get_model():
         nn.LSTM(BEATS, 2, 1)
     )
     return model
+
+
+def calculate_acc(y_pred, y):
+    tp, fp, tn, fn = 0, 0, 0, 0
+    for i in range(y_pred.shape[0]):
+        if y_pred[i] == 0:
+            if y[i] == 0:
+                tn += 1
+            else: fn += 1
+        elif y[i] == 0:
+            fp += 1
+        else:
+            tp += 1
+    return tp, fp, tn, fn
 
 
 if __name__ == '__main__':
