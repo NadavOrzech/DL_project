8573d10a93cb70ae0d35347aacdee09942258bd8
englender <ronien262@gmail.com>
2020-09-05 18:33:57 +0300

    added testset

--- source/data_loader.py
+++ source/data_loader.py
@@ -1,8 +1,9 @@
 import torch.utils.data
 import torch
-from data_preprocess import get_data
+from data_preprocess import get_data, SEQ_SIZE
 
 BATCH_SIZE = 4
+TRAIN_TEST_RATIO = 0.9
 
 
 def get_dataloader():
@@ -10,12 +11,21 @@ def get_dataloader():
     xx = torch.tensor(xx, dtype=torch.float32)
     xx = torch.flatten(xx, start_dim=2) # TODO: maybe flatten differnetly - do first column then second column (and not first second first second..)
     yy = torch.tensor(yy, dtype=torch.float32)
-    dataset = torch.utils.data.TensorDataset(xx, yy)
-    sampler = torch.utils.data.SequentialSampler(dataset)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, sampler=sampler, shuffle=False)
-    return dataloader
+
+    num_samples = xx.shape[0]
+    num_train = int(TRAIN_TEST_RATIO * num_samples)
+    train_dataset = torch.utils.data.TensorDataset(xx[:num_train], yy[:num_train])
+    train_sampler = torch.utils.data.SequentialSampler(train_dataset)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, sampler=train_sampler,
+                                                   shuffle=False, drop_last=True)
+
+    test_dataset = torch.utils.data.TensorDataset(xx[num_train:], yy[num_train:])
+    test_sampler = torch.utils.data.SequentialSampler(test_dataset)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, sampler=test_sampler,
+                                                  shuffle=False, drop_last=True)
+    return train_dataloader, test_dataloader
 
 
 if __name__ == '__main__':
-    dataloader = get_dataloader()
-    print(f"num batches: {len(dataloader)}")
+    train_dataloader, test_dataloader = get_dataloader()
+    print(f"num batches: {len(train_dataloader)}")
--- source/training.py
+++ source/training.py
@@ -9,9 +9,11 @@ INPUT_SIZE = (100, 4, 1000)
 HIDDEN_SIZE = (100, 400)
 BATCH_SIZE = 4
 BEATS = BEAT_SIZE*2
+NUM_EPOCHS = 4
 
 
 def train(model, optimizer, loss_fn, dataloader, max_epochs=4, max_batches=200):
+    print("-----------------------------Starting training------------------------------")
     for epoch_idx in range(max_epochs):
         total_loss, num_correct = 0, 0
         start_time = time.time()
@@ -52,6 +54,41 @@ def train(model, optimizer, loss_fn, dataloader, max_epochs=4, max_batches=200):
         print(f"Negative accuracy: {tn_tot / (tn_tot + fp_tot)}")
 
 
+def test(model, loss_fn, dataloader, max_epochs=4, max_batches=200):
+    print("-----------------------------Starting testing------------------------------")
+    for epoch_idx in range(max_epochs):
+        total_loss, num_correct = 0, 0
+        start_time = time.time()
+        tp_tot, fp_tot, tn_tot, fn_tot = 0, 0, 0, 0
+        for batch_idx, batch in enumerate(dataloader):
+            X, y = batch[0], batch[1]
+
+            # Forward pass
+            X = torch.transpose(X, dim0=0, dim1=1)
+            y_pred_log_proba, hidden_dims = model(X)
+            last_output = y_pred_log_proba[-1]
+            y = torch.squeeze(y).long()
+
+            loss = loss_fn(last_output, y)
+
+            total_loss += loss.item()
+            y_pred = torch.argmax(last_output, dim=1)
+            tp, fp, tn, fn = calculate_acc(y_pred, y)
+            tp_tot += tp
+            fp_tot += fp
+            tn_tot += tn
+            fn_tot += fn
+            num_correct += torch.sum(y_pred == y).float().item()
+            total_samp = tp_tot + tn_tot + fp_tot + fn_tot
+
+        print(
+            f"Epoch #{epoch_idx}, loss={total_loss / (total_samp / BATCH_SIZE):.3f}, accuracy={num_correct / total_samp:.3f}, elapsed={time.time() - start_time:.1f} sec")
+        print(f"tp: {tp_tot}, fp: {fp_tot}, tn: {tn_tot}, fn: {fn_tot}")
+        if tp_tot + fn_tot > 0:
+            print(f"Positive accuracy: {tp_tot / (tp_tot + fn_tot)}")
+        print(f"Negative accuracy: {tn_tot / (tn_tot + fp_tot)}")
+
+
 def get_model():
     model = nn.Sequential(
         nn.LSTM(BEATS, 2, 1)
@@ -74,9 +111,10 @@ def calculate_acc(y_pred, y):
 
 
 if __name__ == '__main__':
-    dataloader = get_dataloader()
+    train_dataloader, test_dataloader = get_dataloader()
     model = get_model()
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
-    train(model, optimizer, loss_fn, dataloader)
+    train(model, optimizer, loss_fn, train_dataloader, max_epochs=NUM_EPOCHS)
+    test(model, loss_fn, test_dataloader, max_epochs=NUM_EPOCHS)
 
